80f4cb0f6f37713095b12257ef90cae1330ea5fb
argen <argen.adenov@emoldino.com>
2023-12-19 17:50:09 +0900

    update ui

--- ui/js/main.js
+++ ui/js/main.js
@@ -36,6 +36,38 @@ function applyFilters() {
     });
 }
 
+function hideTdo() {
+  var timer = null;
+  var target = document.querySelector('#tidio-chat iframe');
+  if(!target) {
+    if(timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(hideTdo, 500);
+    return;
+  } else {
+    var timer2 = null;
+    var tdo = document.querySelector('#tidio-chat iframe')
+                .contentDocument
+                .querySelector('a[href*="tidio.com/powered"]');
+    if(!tdo) {
+      if(timer2 !== null) {
+        clearTimeout(timer2);
+      }
+      timer2 = setTimeout(hideTdo, 1);
+      return;
+    }
+    document.querySelector('#tidio-chat iframe')
+      .contentDocument
+      .querySelector('a[href*="tidio.com/powered"]')
+      .remove();
+    return true;
+  }
+}
+
+hideTdo();
+
+setInterval(hideTdo, 10);
 
     // Event handlers for filter inputs
     $('#modelSearch, #versionFilter, #modelFilter, #gbFilter').on('input change', applyFilters);
